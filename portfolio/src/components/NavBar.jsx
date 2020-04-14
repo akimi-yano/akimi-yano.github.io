f722bbab7bd2715c4ec5d082ff427b9c49962e9e
Akimi Yano <akimi.mimi.yano@gmail.com>
2020-04-14 01:30:05 -0700

    addedd new changes

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -27,11 +27,11 @@ const NavBar = () => {
             <nav className="noboxshadow banner-hidden">
                 <div className="nav-wrapper clear" >
                 {/* <a href="" data-activates="mobile-demo" className="button-collapse"><ViewListIcon/></a> */}
-                    <a href="" className="brand-logo raleway hide-on-med-and-down" style={{color:'black', margin: '0 20px'}}>Akimi Yano<span className="tealfont"></span></a>
+                    <a href="" className="titleName" style={{color:'black', margin: '0 20px'}}>Akimi Yano<span className="tealfont"></span></a>
                     
                     <ul style={{margin: '0 20px'}} className="right hide-on-med-and-down">
-                        <li ><a className="navlink blackfont aboutbtn" href="#about"><PersonIcon style={{marginBottom:'-4px'}}/> About </a></li>
-                        <li  style={{width: '15vw', textAlign:'center'}} className="projectOption"><a className="navlink blackfont projbtn dropdown-button"><CreateIcon style={{marginBottom:'-4px'}}/> Projects <ArrowDropDownIcon style={{marginBottom:'-4px'}}/></a>
+                        <li style={{width: '15vw', textAlign:'center'}} ><a className="navlink blackfont aboutbtn" href="#about"><PersonIcon style={{marginBottom:'-4px'}}/> About </a></li>
+                        <li style={{width: '15vw', textAlign:'center'}} className="projectOption"><a className="navlink blackfont projbtn dropdown-button"><CreateIcon style={{marginBottom:'-4px'}}/> Projects <ArrowDropDownIcon style={{marginBottom:'-4px'}}/></a>
                         <ul id="dropdown1" className="dropdown-content">
                 <li><a id="project1btn" href="#project1" className="blackfont">1. Face Labo</a></li>
                 <li><a id="project2btn" href="#project2" className="blackfont">2. ice candi</a></li>
@@ -39,8 +39,8 @@ const NavBar = () => {
                 <li><a id="project4btn" href="#project4" className="blackfont">4. Climbing Analysis</a></li>
             </ul>
                         </li>
-                        <li ><a className="navlink blackfont techbtn" href="#skills"><SettingsIcon style={{marginBottom:'-4px'}}/> Skills </a></li>
-                        <li ><a className="navlink blackfont contactbtn" href="#contact"><EmailIcon style={{marginBottom:'-4px'}}/> Contact </a></li>
+                        <li style={{width: '15vw', textAlign:'center'}} ><a className="navlink blackfont techbtn" href="#skills"><SettingsIcon style={{marginBottom:'-4px'}}/> Skills </a></li>
+                        <li style={{width: '15vw', textAlign:'center'}} ><a className="navlink blackfont contactbtn" href="#contact"><EmailIcon style={{marginBottom:'-4px'}}/> Contact </a></li>
                     </ul>
                 </div>
             </nav>
